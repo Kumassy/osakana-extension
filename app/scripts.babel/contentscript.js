bb91ede69d5c38fee7bff638a9a02add1cba995c
Kumassy <kumassyii@gmail.com>
2017-10-29 21:29:53 +0900

    resolve #3

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -28,16 +28,44 @@
     document.querySelector('table tr strong').parentElement.parentElement.classList.add('danger');
   }
   else if (window.location.href.match(/http:\/\/osakana.ailab.ics.keio.ac.jp\/histories\/\d+/)) {
+    const history_styles = `
+      <style>
+        thead th, thead td {
+          cursor: pointer;
+        }
+        thead th:after, thead td:after {
+          content: '\\f0dc';
+          font-family: FontAwesome;
+          font-size: 12px;
+          color: #ccc;
+          float: right;
+          padding-top: 4px;
+          font-weight: normal;
+        }
+        thead th.heaven-extension-asc:after, thead td.heaven-extension-asc:after {
+          content: '\\f0de';
+          font-family: FontAwesome;
+          color: #000;
+        }
+        thead th.heaven-extension-desc:after, thead td.heaven-extension-desc:after {
+          content: '\\f0dd';
+          font-family: FontAwesome;
+          color: #000;
+        }
+      </style>
+    `;
+    document.querySelector('head').insertAdjacentHTML('beforeend', history_styles);
+
     chrome.storage.sync.get({
       username: ''
     }, function(items) {
       let username = '';
-      const table = Array.from(document.querySelector('table tbody').children);
+      let rows = Array.from(document.querySelector('table tbody').children);
 
       if (items.username === '') {
         // detect username
         const counter = {};
-        table.forEach((tr) => {
+        rows.forEach((tr) => {
           const name1 = tr.children[1].innerText;
           const name2 = tr.children[2].innerText;
 
@@ -65,37 +93,87 @@
       console.log(username);
 
       // swap table columns so that 'username' comes to leftmost
-      table.forEach((tr) => {
-        const name1 = tr.children[1].innerText;
-        const name2 = tr.children[2].innerText;
+      rows.forEach((row) => {
+        const name1 = row.children[1].innerText;
+        const name2 = row.children[2].innerText;
 
         if (name2 === username) {
           // need swap
-          tr.children[1].innerText = name2;
-          tr.children[2].innerText = name1;
+          row.children[1].innerText = name2;
+          row.children[2].innerText = name1;
 
-          const score1 = tr.children[3].innerText;
-          const score2 = tr.children[4].innerText;
-          tr.children[3].innerText = score2;
-          tr.children[4].innerText = score1;
+          const score1 = row.children[3].innerText;
+          const score2 = row.children[4].innerText;
+          row.children[3].innerText = score2;
+          row.children[4].innerText = score1;
         }
       });
 
+      // add diff column
+      document.querySelector('thead tr').insertAdjacentHTML('beforeend', '<td>diff</td>');
+      rows.forEach((row) => {
+        const score1 = parseInt(row.children[3].innerText);
+        const score2 = parseInt(row.children[4].innerText);
+        row.insertAdjacentHTML('beforeend', `<td>${score1 - score2}</td>`);
+      });
+
       // highlight rows
-      table.forEach((tr) => {
-        const score1 = parseInt(tr.children[3].innerText);
-        const score2 = parseInt(tr.children[4].innerText);
+      rows.forEach((row) => {
+        const score1 = parseInt(row.children[3].innerText);
+        const score2 = parseInt(row.children[4].innerText);
 
         if (score1 === score2) {
           // draw
-          tr.classList.add('active');
+          row.classList.add('active');
         } else if (score1 > score2) {
           // win
-          tr.classList.add('success');
+          row.classList.add('success');
         } else {
           // lose
-          tr.classList.add('danger');
+          row.classList.add('danger');
         }
+      });
+
+      // sort
+      const typemap = ['number', 'string', 'string', 'number', 'number', 'number'];
+      let sortOrder = 1;
+      const header = Array.from(document.querySelector('thead tr').children);
+      const tbody = document.querySelector('table tbody');
+      header.forEach((h) => {
+        h.addEventListener('click', () => {
+          let rows = Array.from(document.querySelector('table tbody').children);
+          const col = h.cellIndex;
+
+          rows = rows.sort((a, b) => {
+            let _a = a.children[col].textContent;
+            let _b = b.children[col].textContent;
+            if (typemap[col] === 'number') {
+              _a = parseInt(_a);
+              _b = parseInt(_b);
+            } else if (typemap[col] === 'string') {
+              _a = _a.toLowerCase();
+              _b = _b.toLowerCase();
+            }
+            if (_a < _b) {
+              return -1 * sortOrder;
+            }
+            if (_a > _b) {
+              return 1 * sortOrder;
+            }
+            return 0;
+          });
+
+          // update dom
+          while (tbody.firstChild) {
+            tbody.removeChild(tbody.firstChild);
+          }
+          rows.forEach((row) => {
+            tbody.appendChild(row);
+          });
+
+          h.className = sortOrder === 1 ? 'heaven-extension-asc' : 'heaven-extension-desc';
+          sortOrder *= -1;
+        });
       });
     });
   }
